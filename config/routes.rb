7f6c126e768329665df86c0e2492912f43d16f36
yuki <mark2peace@gmail.com>
2024-09-14 19:54:54 +0900

    add:ルーティング仮保存

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api, format: 'json' do
+    resources :spend, only: %i[index create destroy] do
+      collection do
+        delete 'selected_delete'
+      end
+    end
+    resources :spend_category, only: %i[index create destroy update]
+  end
 end
