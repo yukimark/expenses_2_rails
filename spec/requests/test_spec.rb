f18cfd7a0b7a005194b70f888aa7489bf4cb8973
yuki <mark2peace@gmail.com>
2024-09-18 17:11:19 +0900

    add:test controllerでサンプルのrspec記述

--- spec/requests/test_spec.rb
+++ spec/requests/test_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Tests', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      get '/api/test/index'
+    end
+
+    it '成功レスポンスが返ってくる' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'レスポンスjsonのmessageがテスト' do
+      json = JSON.parse(response.body)
+      expect(json['message']).to eq('テスト')
+    end
   end
 end
